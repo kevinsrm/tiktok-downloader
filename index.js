e76d0ae6eb6b583be36cc7b45dbc4b64dce6b0d7
kevin srm <kevinborrachao@gmail.com>
2025-07-17 16:13:25 -0300

    adicionado variaveis de ambiente

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require("express")
 const routes = require("./routes")
 const app = express()
+const port = process.env.PORT || 3000;
 
 
 app.use("/api", routes)
 
 
 
-app.listen(3000,()=>{console.log("servidor rodando na porta 3000")})
+app.listen(port,()=>{console.log("servidor rodando na porta: " + port)})
 
